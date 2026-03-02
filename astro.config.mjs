a90b9373a6f05bead1c0aedbfcf2d9a6561aed02
edgardo.cadena-at-888633233994 <edgardo.cadena@covaf.com>
2026-03-01 19:41:00 -0600

    dominio para enviar correos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,20 @@ import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://yourdomain.com",
+  site: "https://seo-muestra.vercel.app",
   output: "static",
-  adapter: vercel()
+  adapter: vercel(),
+  security: {
+    checkOrigin: true,
+    allowedDomains: [
+      {
+        protocol: "https",
+        hostname: "seo-muestra.vercel.app"
+      },
+      {
+        protocol: "https",
+        hostname: "**.vercel.app"
+      }
+    ]
+  }
 });
